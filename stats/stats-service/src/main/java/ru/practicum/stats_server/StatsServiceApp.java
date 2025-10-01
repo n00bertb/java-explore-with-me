5c00b9d1b0406c2f9ca5b75be1d663df03c3821b
Артём Самуйлов <artemka.samuiloff@yandex.ru>
2025-10-02 01:20:09 +0300

    удалил лишний импорт -_-

--- stats/stats-service/src/main/java/ru/practicum/stats_server/StatsServiceApp.java
+++ stats/stats-service/src/main/java/ru/practicum/stats_server/StatsServiceApp.java
@@ -1,8 +1,6 @@
 package ru.practicum.stats_server;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 
 public class StatsServiceApp {
     public static void main(String[] args) {
