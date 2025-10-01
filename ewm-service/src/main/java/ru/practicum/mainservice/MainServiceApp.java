1eeb8148524b123f16686002e42a349e55739840
Артём Самуйлов <artemka.samuiloff@yandex.ru>
2025-10-02 02:17:56 +0300

    и еще стилистические правки

--- ewm-service/src/main/java/ru/practicum/mainservice/MainServiceApp.java
+++ ewm-service/src/main/java/ru/practicum/mainservice/MainServiceApp.java
@@ -3,7 +3,7 @@ package ru.practicum.mainservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"ru.practicum.stats_client", "ru.practicum.mainservice"})
+@SpringBootApplication(scanBasePackages = {"ru.practicum.statsclient", "ru.practicum.mainservice"})
 public class MainServiceApp {
     public static void main(String[] args) {
         SpringApplication.run(MainServiceApp.class, args);
